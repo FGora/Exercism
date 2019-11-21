ad2ca1a52fa192a17a458add8c3b5df18a63eaa2
Felix Gora <felix.gora@iCloud.com>
2019-11-21 14:58:31 +0100

    raindrops solution

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
 def convert(number):
-    pass
+    res = ""
+    if number % 3 ==0:
+        res = res + "Pling"
+    if number % 5 ==0:
+        res = res + "Plang"
+    if number % 7 ==0:
+        res = res + "Plong"   
+    if res == "":
+        return str(number)
+    return res         
