8e31c04b67142835cdfe495db0b8bde42a2cc5b4
Felix Gora <felix.gora@iCloud.com>
2019-11-21 18:52:40 +0100

    solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,13 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        entrys=[]
+        rows=matrix_string.split("\n")
+        for r in range(len(rows)):
+            entrys.append([int(entry) for entry in rows[r].split(" ")])
+        self.entrys = entrys
 
     def row(self, index):
-        pass
+        return self.entrys[index-1]
 
     def column(self, index):
-        pass
+        return [row[index-1] for row in self.entrys] 
